572c51cc0b76dda6ebecfe15ca210e162d9922b5
Randy Thiemann <uselinuxnow@gmail.com>
2014-10-15 22:23:45 +0200

    Aha.

--- 01ParPi/build.sh
+++ 01ParPi/build.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
+unamestr=`uname`
+if [[ "$unamestr" == 'Darwin' ]]; then
+    OPTS='.. -DCMAKE_CXX_COMPILER=g++-4.9 -DCMAKE_C_COMPILER=gcc-4.9'
+else
+    OPTS='..'
+fi
+
 cd other
 mkdir build
 cd build
-cmake ..
+cmake $OPTS
 make
 cd ..
 cd ..
 cd parfor
 mkdir build
 cd build
-cmake ..
+cmake $OPTS
 make
 cd ..
 cd ..
 cd reduction
 mkdir build
 cd build
-cmake ..
+cmake $OPTS
 make
 cd ..
 cd ..
