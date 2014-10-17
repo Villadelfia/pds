ecd46e6ddeb3f14ae26d87cd6c59f104edb64e7d
Randy Thiemann <uselinuxnow@gmail.com>
2014-10-17 23:16:37 +0200

    Make it runnable.

--- 02ConvMP/build.sh
+++ 02ConvMP/build.sh
@@ -1,30 +1,12 @@
 #!/bin/bash
 unamestr=`uname`
 if [[ "$unamestr" == 'Darwin' ]]; then
-    OPTS='.. -DCMAKE_CXX_COMPILER=g++-4.9 -DCMAKE_C_COMPILER=gcc-4.9'
+    OPTS='../src -DCMAKE_CXX_COMPILER=g++-4.9 -DCMAKE_C_COMPILER=gcc-4.9'
 else
-    OPTS='..'
+    OPTS='../src'
 fi
 
-cd other
 mkdir build
 cd build
 cmake $OPTS
 make
-cd ..
-cd ..
-cd parfor
-mkdir build
-cd build
-cmake $OPTS
-make
-cd ..
-cd ..
-cd reduction
-mkdir build
-cd build
-cmake $OPTS
-make
-cd ..
-cd ..
-
--- 02ConvMP/clean.sh
+++ 02ConvMP/clean.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 rm output.txt
-cd other
 rm -rf build
-cd ..
-cd reduction
-rm -rf build
-cd ..
-cd parfor
-rm -rf build
-cd ..
